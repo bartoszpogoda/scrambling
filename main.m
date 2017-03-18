8595ef2dbb88aece3eaddf76c7805efcaa8c55b5
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-03-18 01:05:50 +0100

    główny skrypt i widok aplikacji

---
+++ main.m
@@ -0,0 +1,8 @@
+
+%odpalenie g³ównego widoku aplikacji - czyli wywolanie funkcji
+%zdefiniowanej w glownyWidok.m, która z kolei odpala figure
+%glownyWidok.fig, stworzon¹ przy pomocy GUIDE (lewym na glownyWidok.fig i
+%edytuj w GUIDE)
+glownyWidok();
+
+
