3d2f3740e71b8d19e5452db8797c4f3e56c2f31b
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-04-12 20:07:02 +0200

    stworzona klasa obudowujaca bajt danych, podstawowa implementacja

--- main.m
+++ main.m
@@ -7,4 +7,15 @@ addpath(genpath('widoki'));
 %edytuj w GUIDE)
 glownyWidok();
 
+% demo BajtDanych
+bajt = BajtDanych();    % inicjowany jest zerem
+bajt.setBit(2);         % ustawia drugi bit w sekwencji na 1
+bajt.getBit(2)          % zwroci do Command Window 1, poniewa¿ bit zosta³ ustawiony
+bajt.clearBit(2);       % ustawia drugi bit w sekwencji na 0
+bajt.getBit(2)          % zwroci do Command Window 0, poniewa¿ bit zosta³ wyczyszczony
+bajt.setBitV(1,1);      % ustawi pierwszy bit na 1
+bajt.setBitV(5,1);      % ustawi pi¹ty bit na 1
+bajt.setBitV(5,0);      % wyczyœci pi¹ty bit
+bajt                    % wypisze obiekt, z zawartoœci¹ Bajtu czyli 2 (01000000)
+
 
