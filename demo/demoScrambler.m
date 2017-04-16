ebf778dcdf995270422397a9ee452f3d03df9aac
SJantos <sebastian.jantos@hotmail.com>
2017-04-16 21:57:20 +0200

    Working scrambler and descrambler

--- demo/demoScrambler.m
+++ demo/demoScrambler.m
@@ -1,26 +1,26 @@
 addpath(genpath('model'));
 
 % demo Sygnal
-sygnal = Signal(10);             % tworzony sygna³ o d³ugoœci 10 bitów
+sygnal = Signal(20);             % tworzony sygna³ o d³ugoœci 10 bitów
 sygnal.setBit(2);                % ustawiony 2-gi bit (wagi 2^1)
 sygnal.setBit(7);                % ustawiony 7-dmy bit (wagi 2^6)  
 sygnal.setBit(10);               % ustawiony 10-ty bit (wagi 2^9)  
-% sygnal.setBit(15);
-% sygnal.setBit(16);
-% sygnal.setBit(17);
-% sygnal.setBit(5);
-% sygnal.setBit(19);
-% sygnal.setBit(11);
+sygnal.setBit(15);
+sygnal.setBit(16);
+sygnal.setBit(17);
+sygnal.setBit(5);
+sygnal.setBit(19);
+sygnal.setBit(11);
 
 sygnal2 = sygnal;
 % ..0001000010
 
 scrambler = Scrambler();
-descrambler = Scrambler();
+descrambler = Descrambler();
 
 disp("pocz¹tkowy sygnal"); sygnal.disp();
 sygnal2 = scrambler.scramble(sygnal2);
 disp("po scramblerze"); sygnal2.disp();
-disp(descrambler.LFSR);
+%disp(descrambler.LFSR);
 sygnal2 = descrambler.scramble(sygnal2);
 disp("po descramblerze"); sygnal2.disp();
--- model/Descrambler.m
+++ model/Descrambler.m
@@ -1,4 +1,4 @@
-classdef Scrambler
+classdef Descrambler
     %UNTITLED2 Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -8,18 +8,19 @@ classdef Scrambler
     end
     
     methods
-        function obj = Scrambler()
+        function obj = Descrambler()
             Default  = [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1];%randi([0 1],1,59);
             obj.LFSR = Default;
         end
         
         function o = scramble(obj, signalToScramble)
             for i = 1:signalToScramble.getSize()
+                insertBit = signalToScramble.getBit(i);
                 x = xor(obj.LFSR(1,1), xor(obj.LFSR(1,40), obj.LFSR(1,59)));
-                x = xor(signalToScramble.getBit(i), x);
+                x = xor(insertBit, x);
                 signalToScramble.setBitV(i, x);
 
-                obj.LFSR = [signalToScramble.getBit(i), obj.LFSR(1:end-1)];
+                obj.LFSR = [insertBit, obj.LFSR(1:end-1)];
                 
                 o = signalToScramble;
             end
