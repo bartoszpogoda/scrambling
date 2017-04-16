6254971a1649f3718be3e874044f6b5618afca50
SJantos <sebastian.jantos@hotmail.com>
2017-04-16 18:32:43 +0200

    Scrambler try

--- demo/demoScrambler.m
+++ demo/demoScrambler.m
@@ -5,29 +5,22 @@ sygnal = Signal(10);             % tworzony sygna³ o d³ugoœci 10 bitów
 sygnal.setBit(2);                % ustawiony 2-gi bit (wagi 2^1)
 sygnal.setBit(7);                % ustawiony 7-dmy bit (wagi 2^6)  
 sygnal.setBit(10);               % ustawiony 10-ty bit (wagi 2^9)  
-sygnal.clearBit(10);             % wyzerowany 10-ty bit (wagi 2^9) 
-sygnal.setBitV(4,1);             % ustawiony 4-ty bit (wagi 2^3) 
+% sygnal.setBit(15);
+% sygnal.setBit(16);
+% sygnal.setBit(17);
+% sygnal.setBit(5);
+% sygnal.setBit(19);
+% sygnal.setBit(11);
 
 sygnal2 = sygnal;
-sygnal.disp();
-sygnal2.disp();
 % ..0001000010
-%disp("sygnal");
-%sygnal.disp();
 
 scrambler = Scrambler();
-LFSRs = scrambler.LFSR;
-sygnal = scrambler.scramble(sygnal);
-
-disp("scrambler");
-sygnal.disp();
-
 descrambler = Scrambler();
-LFSRd = descrambler.LFSR;
-if(LFSRs == LFSRd)
-    disp("Takie same");
-end
-sygnal2 = descrambler.scramble(sygnal2);
 
-disp("descrambler");
-sygnal2.disp();
+disp("pocz¹tkowy sygnal"); sygnal.disp();
+sygnal2 = scrambler.scramble(sygnal2);
+disp("po scramblerze"); sygnal2.disp();
+disp(descrambler.LFSR);
+sygnal2 = descrambler.scramble(sygnal2);
+disp("po descramblerze"); sygnal2.disp();
--- model/Scrambler.m
+++ model/Scrambler.m
@@ -9,18 +9,18 @@ classdef Scrambler
     
     methods
         function obj = Scrambler()
-            Default  = [0, 1, 1, 0, 0];%, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1];%randi([0 1],1,59);
+            Default  = [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1];%randi([0 1],1,59);
             obj.LFSR = Default;
         end
         
         function o = scramble(obj, signalToScramble)
             for i = 1:signalToScramble.getSize()
-                disp("LFSR"); disp(obj.LFSR);
-                %x = xor(obj.LFSR(1,1), xor(obj.LFSR(1,40), obj.LFSR(1,59)));
-                x = xor(obj.LFSR(1,1), xor(obj.LFSR(1,3), obj.LFSR(1,5)));
+                x = xor(obj.LFSR(1,1), xor(obj.LFSR(1,40), obj.LFSR(1,59)));
                 x = xor(signalToScramble.getBit(i), x);
                 signalToScramble.setBitV(i, x);
-                obj.LFSR = circshift(obj.LFSR, 1, 1);
+
+                obj.LFSR = [signalToScramble.getBit(i), obj.LFSR(1:end-1)];
+                
                 obj.LFSR(1,1) = signalToScramble.getBit(i);
                 o = signalToScramble;
             end
