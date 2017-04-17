173c76812239ab51d3ccf93828ce5b59e90f80ec
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-04-17 20:07:43 +0200

    implemented helper method to align signal sizes to 64

--- demo/demoEthernet.m
+++ demo/demoEthernet.m
@@ -1,10 +1,11 @@
-    addpath(genpath('model'));
+addpath(genpath('model'));
+addpath(genpath('helper'));
 
 % demo Ethernet
 ethernetCoder = EthernetCoder();
 
 % parameters
-signalSize = 128;
+signalSize = 113;
 
 % create a random signal
 signal = Signal(signalSize);
@@ -14,11 +15,16 @@ for i = 1 : signalSize
     end
 end
 
+% append zeroes to match ethernet 64-bit framing
+signal = Helper.appendToAlign64(signal);
+
 % encoding demo
 disp("Signal before encoding:");
 signal.disp();
+fprintf("Size: %d \n", signal.getSize());
 
-encodedSignal = ethernetCoder.encode(signal);
+signal = ethernetCoder.encode(signal);
 
 disp("Signal after encoding:");
-encodedSignal.disp();
+signal.disp();
+fprintf("Size: %d \n", signal.getSize());
