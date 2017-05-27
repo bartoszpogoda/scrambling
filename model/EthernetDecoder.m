80903d253a06c7129999ee70179a70d54ee5af50
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-05-27 13:34:23 +0200

    work on resync algorithm- imlemented right and left lookup for possible preambles

--- model/EthernetDecoder.m
+++ model/EthernetDecoder.m
@@ -2,10 +2,76 @@ classdef EthernetDecoder < handle
     %ETHERNETDECODER Summary of this class goes here
     
     properties (Access = private)
-        errorFlag, badFrameIndex
+        errorFlag, badFrameIndex,
+        resyncPreamblesToCheck = 3, % number of preambles to check both to the left and right side
+        resyncPreambleCheckRange = 3
     end
     
     methods
+        function dataIndex = resync(obj, signal, badFrameIndex)
+            % scores for preambles are 0, and they are not yet found so
+            % indexex are -1
+            potentialPreamblesScores = zeros(1,2*obj.resyncPreamblesToCheck);
+            potentialPreamblesIndexes = zeros(1,2*obj.resyncPreamblesToCheck);
+            
+            for i = 1 : 2*obj.resyncPreamblesToCheck
+                potentialPreamblesIndexes(i) = -1;
+            end
+            
+            % go left
+            iterator = badFrameIndex - 1;
+            foundPreamblesLeft = 0;
+            
+            while iterator >= 1 && foundPreamblesLeft ~= obj.resyncPreamblesToCheck
+                while ~(signal.getBit(iterator) == 0 && signal.getBit(iterator+1)) && iterator >= 1
+                    iterator = iterator - 1;
+                end
+                
+                if iterator >= 1
+                    % found '01' preamble
+                    foundPreamblesLeft = foundPreamblesLeft + 1;
+                    potentialPreamblesIndexes(foundPreamblesLeft) = iterator;
+                    
+                    % score logic
+                    score = 53151531531;
+                    
+                    % save score
+                    potentialPreamblesScores(foundPreamblesLeft) = score;
+                    
+                    iterator = iterator - 1;
+                end
+            end
+            
+            % go right
+            iterator = badFrameIndex + 1;
+            foundPreamblesRight = 0;
+            
+            while iterator < signal.getSize() && foundPreamblesRight ~= obj.resyncPreamblesToCheck
+                while iterator < signal.getSize() && ~(signal.getBit(iterator) == 0 && signal.getBit(iterator+1)) 
+                    iterator = iterator + 1;
+                end
+                
+                if iterator < signal.getSize()
+                    % found '01' preamble
+                    foundPreamblesRight = foundPreamblesRight + 1;
+                    potentialPreamblesIndexes(foundPreamblesRight + foundPreamblesLeft) = iterator;
+                    
+                    % score logic
+                    score = 53151531531;
+                    
+                    % save score
+                    potentialPreamblesScores(foundPreamblesRight + foundPreamblesLeft) = score;
+                    
+                    iterator = iterator + 1;
+                end
+            end
+            
+            
+            potentialPreamblesIndexes
+            
+            dataIndex = 12141512;
+        end
+        
         function decodedSignal = decode(obj, signal)
             % clear previous error flag
             obj.errorFlag = false;
@@ -23,7 +89,12 @@ classdef EthernetDecoder < handle
                 % check for preamble
                 if signal.getBit(i) ~= 0 || signal.getBit(i+1) ~= 1
                     obj.errorFlag = true;
-                    obj.badFrameIndex = currentFrame;
+                    obj.badFrameIndex = i;
+                    
+                    %dbg---
+                    obj.resync(signal, i);
+                    %dbg---
+                    
                     return;
                 end
                     
